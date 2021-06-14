72f6d3c19fd3d2d46b69f2dd38e9ad33f2a29194
baa <34665120+baa-mb@users.noreply.github.com>
2021-06-15 00:07:02 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,11 +2,13 @@ input.onButtonPressed(Button.A, function () {
     basic.showNumber(kopf)
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showNumber(10000 - kopf)
+    basic.showNumber(anzahl - kopf)
 })
 let kopf = 0
+let anzahl = 0
+anzahl = 10000
 kopf = 0
-for (let index = 0; index < 10000; index++) {
+for (let index = 0; index < anzahl; index++) {
     if (randint(0, 1) == 0) {
         kopf += 1
     }
